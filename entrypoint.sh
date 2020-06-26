699f36f5b1c99aaba7526d6b7e1b5e45a09dfcee
José Leonan S. Carvalho <j.leonancarvalho@gmail.com>
2020-06-26 17:42:53 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 composer --working-dir=$(echo $(pwd)/src) update --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 npm install
 grunt build_linux
